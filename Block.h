4ae9ba46ca9f4ba5528004fb7d1d23bde3eef27e
BotBw <wang1570@e.ntu.edu.sg>
2023-02-26 00:17:02 +0800

    lazy allcoate

--- Block.h
+++ Block.h
@@ -3,11 +3,7 @@
 
 const int BLOCK_SIZE = 200;
 // use MB instead of MiB
-#ifdef DEBUG
-const int DISK_MEM_SIZE = 1 * 1000 * 1000;
-#else
 const int DISK_MEM_SIZE = 500 * 1000 * 1000;
-#endif
 using Block = char[BLOCK_SIZE];
 
 #endif //BPTREE_BLOCK_H
--- Disk.hpp
+++ Disk.hpp
@@ -12,33 +12,25 @@ using namespace std;
 template<typename T>
 class Disk {
 private:
-    int poolSize;
+    const int poolSize;
+    const int numBlock;
+    const int numTPerBlk;
     // total pool size in our simulated disk
     char *pool;
     // available T*
-    queue<T *> avail;
+    queue<T *> unallocated;
     // allocated T*
     unordered_set<T *> allocated;
     // from node to block
     unordered_map<T *, Block *> blkOf;
+    // the index of first available block
+    int blkId;
 
 public:
-    Disk(int _poolSize) : poolSize{_poolSize}, pool{new char[poolSize]} {
+    Disk(int _poolSize) : poolSize{_poolSize}, numBlock{poolSize / (int) sizeof(Block)},
+                          numTPerBlk{sizeof(Block) / sizeof(T)}, pool{new char[poolSize]}, blkId{0} {
         assert(poolSize >= sizeof(Block));
         assert(sizeof(Block) >= sizeof(T));
-        int numBlock = poolSize / sizeof(Block);
-        int numTPerBlk = sizeof(Block) / sizeof(T);
-        Block *blkStart = (Block *) pool;
-        for (int i = 0; i < numBlock; i++) {
-            Block *curBlk = blkStart + i;
-            T *tStart = (T *) curBlk;
-            for (int j = 0; j < numTPerBlk; j++) {
-                T *curT = tStart + j;
-                avail.push(curT);
-                blkOf[curT] = curBlk;
-            }
-        }
-        assert(avail.size() == numBlock * numTPerBlk);
     }
 
     ~Disk() {
@@ -46,10 +38,22 @@ public:
     }
 
     T *allocate(const T &val) {
-        if(avail.empty()) throw runtime_error("No enough space in the disk!");
-        T *ret = avail.front();
-        avail.pop();
-        new (ret) T(val);
+        if (unallocated.empty()) { // no available space, try to lazily get blocks
+            if (blkId == numBlock) throw runtime_error("No enough space in the disk!"); // no block and no
+            // if there are blocks left
+            Block *blkStart = (Block *) pool;
+            Block *curBlk = blkStart + blkId;
+            T *tStart = (T *) curBlk;
+            for (int j = 0; j < numTPerBlk; j++) {
+                T *curT = tStart + j;
+                unallocated.push(curT);
+                blkOf[curT] = curBlk;
+            }
+            blkId++;
+        }
+        T *ret = unallocated.front();
+        unallocated.pop();
+        new(ret) T(val);
         assert(allocated.count(ret) == 0);
         allocated.insert(ret);
         return ret;
@@ -58,31 +62,22 @@ public:
     void deallocate(T *p) {
         assert(allocated.count(p) != 0);
         allocated.erase(p);
-        avail.push(p);
+        unallocated.push(p);
     }
 
     int countAccessed(vector<T *> node) {
         //  a set counter for blocks
-        set<Block *> cntBlock;
+        unordered_set<Block *> cntBlock;
         for (auto it = node.begin(); it != node.end(); it++) {
             Block *blk = blkOf[*it];
             cntBlock.insert(blk);
         }
         return cntBlock.size();
     }
-//    int countUsed()
-//    {
-//        int cnt = 0;
-//        for (int i = 0; i < totalCnt; i++)
-//        {
-//            curBlk = (Block *)pool + i;
-//            if (left[curBlk] == sizeof(Block))
-//            {
-//                cnt++;
-//            }
-//        }
-//        return cnt;
-//    }
+    int countUsed()
+    {
+        return allocated.size();
+    }
 };
 
 #endif // BPTREE_DISK_HPP
