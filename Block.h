1acfb47cceb14efb9717162160d4c38f129e090f
BotBw <wang1570@e.ntu.edu.sg>
2023-03-04 21:58:39 +0800

    finalize

--- Block.h
+++ Block.h
@@ -1,6 +1,7 @@
 #ifndef BPTREE_BLOCK_H
 #define BPTREE_BLOCK_H
 
+// each block is 200 bytes (char)
 const int BLOCK_SIZE = 200;
 // use MB instead of MiB
 const int DISK_MEM_SIZE = 500 * 1000 * 1000;
